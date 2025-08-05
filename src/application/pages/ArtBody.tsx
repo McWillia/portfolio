7a8cd361fbb2562a5118d7ba048717f342b79dda
Steven McWilliam <steve-mcw@live.co.uk>
2025-08-05 20:08:35 +0100

    Removed PDF viewer

--- src/application/pages/ArtBody.tsx
+++ src/application/pages/ArtBody.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { PDFViewer } from "../../library/pdf_viewer";
 import { Flex } from "@chakra-ui/react";
 
 const ArtBody = () => {
   return (
     <Flex>
-          <PDFViewer fileUrl="/wall.pdf" />
+      TBC
     </Flex>
   );
 };
